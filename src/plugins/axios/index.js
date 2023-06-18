d36c098ef8a89c40ba3dd017fb35f74f013fd9b9
gillanes92 <46944138+gillanes92@users.noreply.github.com>
2023-06-17 22:42:26 -0400

    2 files modified

--- src/plugins/axios/index.js
+++ src/plugins/axios/index.js
@@ -4,7 +4,7 @@ const baseURL =
     window.location.protocol +
     '//' +
     window.location.host.split(':')[0] +
-    (import.meta.env.DEV ? ':8000' : '')
+    (import.meta.env.DEV ? ':8001' : '')
 const instance = axios.create({
     baseURL,
 })
